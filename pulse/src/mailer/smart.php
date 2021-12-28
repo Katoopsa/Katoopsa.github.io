72a0b3b6f1f8a854874f883d33bff71dc134915d
Ekaterina Pospelova <kat-pospelova@yandex.ru>
2021-12-28 20:50:36 +0300

    finally fixed smart.php

--- pulse/src/mailer/smart.php
+++ pulse/src/mailer/smart.php
@@ -8,7 +8,7 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
@@ -19,7 +19,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('krestyanka97@mail.ru', 'Pulse');   // От кого письмо 
-$mail->addAddress('kat-pospelova@yandex');     // Add a recipient
+$mail->addAddress('kat-pospelova@yandex.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
