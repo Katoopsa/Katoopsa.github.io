bb90b75690b37c829e878b583437731c6c7326b6
Ekaterina Pospelova <kat-pospelova@yandex.ru>
2021-12-28 22:37:59 +0300

    scroll done

--- pulse/src/js/script.js
+++ pulse/src/js/script.js
@@ -101,4 +101,21 @@ $(document).ready(function(){
     return false;
   });
 
+  // Smooth scroll anf page up
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href^='#']").click(function() {
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+
+  new WOW().init();
 });
